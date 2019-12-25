3e8355e6530648abbb57b47f77d8a89e7c4a848d
Lukasz11111 <miklas.lukasz11@gmial.com>
2019-12-25 20:18:49 +0100

    Pliki bazy danych

---
+++ WartosciOdzywczeApp/DB_con.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace WartosciOdzywczeApp
+{
+	class DB_con
+    {
+        public static SQLiteConnection sql_con;
+        private SQLiteCommand sql_cmd;
+        public static string BazaDabychPlik = "baza.db";
+        public static string BazaDanychOn = "Data Source=" + BazaDabychPlik + ";Version=3;New=False;Compress=True;";
+        public SQLiteCommand Polacz()
+        {
+            try
+            {
+                sql_con = new SQLiteConnection(BazaDanychOn);
+                sql_con.Open();
+                sql_cmd = sql_con.CreateCommand();
+            }
+            catch
+            {
+                MessageBox.Show("Błąd otwarcia");
+            }
+            return sql_cmd;
+        }
+        public void Rozlacz()
+        {
+            try
+            {
+                sql_con.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Błąd zamkniecia");
+            }
+        }
+
+    }
+}
---
+++ WartosciOdzywczeApp/Zapytania_sqlit.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WartosciOdzywczeApp
+{
+    class Zapytania_sqlit
+    {
+
+
+        private void conection()
+        {
+            DB_con conection = new DB_con();
+
+        }
+    }
+}
