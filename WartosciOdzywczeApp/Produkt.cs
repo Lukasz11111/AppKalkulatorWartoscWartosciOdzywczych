ef579e5059dd270d1092b2e6bf5dbca408a83313
FilipGrochala <filip.grochala@o2.pl>
2020-01-16 13:59:45 +0100

    Dodanie kalorii (ćwiczenie pull request)

--- WartosciOdzywczeApp/Produkt.cs
+++ WartosciOdzywczeApp/Produkt.cs
@@ -13,19 +13,23 @@ namespace WartosciOdzywczeApp
         private float bialka;
         private float tluszcze;
         private float blonik;
+        private float kalorie;
 
         private float weglowodany_rzeczywista_ilosc;
         private float bialka_rzeczywista_ilosc;
         private float tluszcze_rzeczywista_ilosc;
         private float blonik_rzeczywista_ilosc;
+        private float kalorie_rzeczywista_ilosc;
 
-        public Produkt(string nazwa, float weglowodany, float bialka, float tluszcze, float blonik)
+        public Produkt(string nazwa, float weglowodany, float bialka, float tluszcze, float blonik, float kalorie)
         {
             this.nazwa = nazwa;
             this.Weglowodany = weglowodany;
             this.Bialka = bialka;
             this.Tluszcze = tluszcze;
             this.Blonik = blonik;
+            this.Kalorie = kalorie;
+
         }
 
         public string Nazwa { get => nazwa; set => nazwa = value; }
@@ -33,6 +37,8 @@ namespace WartosciOdzywczeApp
         public float Bialka { get => bialka; set => bialka = value; }
         public float Tluszcze { get => tluszcze; set => tluszcze = value; }
         public float Blonik { get => blonik; set => blonik = value; }
+        public float Kalorie { get => kalorie; set => kalorie = value; }
+        public float Kalorie_rzeczywista_ilosc { get => kalorie_rzeczywista_ilosc; set => kalorie_rzeczywista_ilosc = value; }
         public float Weglowodany_rzeczywista_ilosc { get => weglowodany_rzeczywista_ilosc; set => weglowodany_rzeczywista_ilosc = value; }
         public float Bialka_rzeczywista_ilosc { get => bialka_rzeczywista_ilosc; set => bialka_rzeczywista_ilosc = value; }
         public float Tluszcze_rzeczywista_ilosc { get => tluszcze_rzeczywista_ilosc; set => tluszcze_rzeczywista_ilosc = value; }
--- WartosciOdzywczeApp/StronaDruga.cs
+++ WartosciOdzywczeApp/StronaDruga.cs
@@ -30,30 +30,32 @@ namespace WartosciOdzywczeApp
 
         private void StronaDruga_FormClosed(object sender, FormClosedEventArgs e)
         {
-            
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(textBoxNazwa.Text))
-            try
-            {
-                float weglo, bialka, tluszcze, blonik;
+                try
+                {
+                    float weglo, bialka, tluszcze, blonik, kalorie;
 
-                float.TryParse(textBoxWeglo.Text.Replace(',', '.'), out weglo);
-                float.TryParse(textBoxBialka.Text.Replace(',', '.'), out bialka);
-                float.TryParse(textBoxTluszcze.Text.Replace(',', '.'), out tluszcze);
-                float.TryParse(textBoxBlonik.Text.Replace(',', '.'), out blonik);
-                Produkt produkt = new Produkt(textBoxNazwa.Text, weglo, bialka, tluszcze, blonik);
-                zapisz_produkt(produkt.Nazwa, produkt.Weglowodany, produkt.Bialka, produkt.Tluszcze, produkt.Blonik);
-                label7.Text = "Udało się dodać produkt";
+                    float.TryParse(textBoxWeglo.Text.Replace(',', '.'), out weglo);
+                    float.TryParse(textBoxBialka.Text.Replace(',', '.'), out bialka);
+                    float.TryParse(textBoxTluszcze.Text.Replace(',', '.'), out tluszcze);
+                    float.TryParse(textBoxBlonik.Text.Replace(',', '.'), out blonik);
+                    float.TryParse(textBoxKalorie.Text.Replace(',', '.'), out kalorie);
+                    Produkt produkt = new Produkt(textBoxNazwa.Text, weglo, bialka, tluszcze, blonik, kalorie);
+                    zapisz_produkt(produkt.Nazwa, produkt.Weglowodany, produkt.Bialka, produkt.Tluszcze, produkt.Blonik);
+                    label7.Text = "Udało się dodać produkt";
 
-            }
-            catch
-            {
-                MessageBox.Show("Nie udało się dodać produktu!");
-            }else
-            MessageBox.Show("Nie udało się dodać produktu!\n Nazwa nie może być pusta!");
+                }
+                catch
+                {
+                    MessageBox.Show("Nie udało się dodać produktu!");
+                }
+            else
+                MessageBox.Show("Nie udało się dodać produktu!\n Nazwa nie może być pusta!");
         }
 
         private void zapisz_produkt(string nazwa, float weglo, float bialka, float tluszcze, float blonik)
